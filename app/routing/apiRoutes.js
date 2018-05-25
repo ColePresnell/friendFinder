20e3fa0ed5d31b274bb4c49761f073449f5a63cd
ColePresnell <cole.presnell@gmail.com>
2018-05-24 23:07:08 -0500

    node modules and various updates

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,30 @@
+var friends = require("../data/friends");
+
+module.exports = function(app){
+    // A GET route with the url /api/friends used to display a JSON of all friends
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+    // A POST routes /api/friends used to handle incoming survey results
+    app.post("/api/friends", function(req, res){
+        var newUserSurvey = req.body;
+        console.log(newUserSurvey.name);
+        console.log(newUserSurvey.photo);
+        console.log(newUserSurvey.scores[0]);
+        console.log(newUserSurvey.scores[1]);
+        console.log(newUserSurvey.scores[2]);
+        console.log(newUserSurvey.scores[3]);
+        console.log(newUserSurvey.scores[4]);
+        console.log(newUserSurvey.scores[5]);
+        console.log(newUserSurvey.scores[6]);
+        console.log(newUserSurvey.scores[7]);    
+        console.log(newUserSurvey.scores[8]);
+        console.log(newUserSurvey.scores[9]);
+
+        console.log(newUserSurvey);
+
+        friends.push(newUserSurvey);
+        res.json(newUserSurvey);
+        
+    });
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require("path");
+
+module.exports = function(app) {
+    // A GET Route displays the survey page
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    //A default, catch-all route that leads to home.html which displays the home page
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
