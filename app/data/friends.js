508d1ca3e143a13a143539f547eb7fd1f938864b
ColePresnell <cole.presnell@gmail.com>
2018-05-24 23:57:56 -0500

    updated html and friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,113 +1,113 @@
 var friends = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    name: "Homer Simpson",
+    photo: "http://interactive.nydailynews.com/2016/05/simpsons-quiz/img/simp1.jpg",
     scores: [
         5,
+        2,
         1,
-        4,
-        4,
-        5,
         1,
         2,
+        2,
         5,
-        4,
-        1
+        1,
+        1,
+        3
     ]
 },
 {
-    name: "Jacob Deming",
-    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    name: "Marge Simpson",
+    photo: "http://www.maskerix.com/wp-content/uploads/2017/05/diy-marge-simpson-halloween-costume-idea.jpg",
     scores: [
-        4,
-        2,
+        1,
         5,
-        1,
-        3,
-        2,
-        2,
-        1,
-        3,
-        2
-    ]
-},
-{
-    name: "Jeremiah Scanlon",
-    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-    scores: [
+        5,
+        4,
+        5,
         5,
         2,
-        2,
-        2,
-        4,
-        1,
-        3,
-        2,
+        5,
         5,
         5
     ]
 },
 {
-    name: "Louis T. Delia",
-    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    name: "Bart Simpson",
+    photo: "https://vignette.wikia.nocookie.net/simpsons/images/7/7c/Bart_Simpson_Season_25_Official.jpg/revision/latest?cb=20140511110240",
     scores: [
-        3,
-        3,
-        4,
-        2,
-        2,
         1,
-        3,
-        2,
-        2,
-        3
-    ]
-},
-{
-    name: "Lou Ritter",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-    scores: [
-        4,
-        3,
-        4,
+        1,
+        1,
+        1,
+        1,
         1,
         5,
-        2,
-        5,
-        3,
+        1,
         1,
         4
     ]
 },
 {
-    name: "Jordan Biason",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    name: "Lisa Simpson",
+    photo: "http://en.idrawing.ru/images/artnew/001-simpsons/14/12.jpg",
     scores: [
+        1,
+        5,
+        5,
+        5,
         4,
         4,
+        2,
+        5,
+        5,
+        4
+    ]
+},
+{
+    name: "Barney Gumble",
+    photo: "https://static.comicvine.com/uploads/square_medium/11/114183/5154902-barney_gumble_-_shading.png",
+    scores: [
+        5,
+        3,
+        1,
+        1,
         2,
         3,
-        2,
-        2,
+        5,
+        1,
+        1,
+        2
+    ]
+},
+{
+    name: "Ned Flanders",
+    photo: "https://vignette.wikia.nocookie.net/simpsons/images/d/d6/Ned_Flanders_2.png/revision/latest?cb=20141024230457",
+    scores: [
+        1,
+        5,
+        5,
+        3,
+        5,
         3,
         2,
-        4,
+        5,
+        5,
         5
     ]
 },
 {
-    name: "Sue",
-    photo: "https://www.rd.com/wp-content/uploads/2018/02/19_Adorable-Puppy-Pictures-that-Will-Make-You-Melt_391191067_chris_tina-760x506.jpg",
+    name: "Chief Wiggum",
+    photo: "https://vignette.wikia.nocookie.net/simpsons/images/0/0e/245px-Chief_Wiggum.png/revision/latest?cb=20140830013250",
     scores: [
+        2,
+        2,
+        2,
+        1,
         3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3
+        2,
+        5,
+        2,
+        1,
+        4
     ]
 }
 ];
